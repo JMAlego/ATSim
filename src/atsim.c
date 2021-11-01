27041c2ef5917e592a985f5f710401425d8ea822
Jacob Allen <jacob@polyomino.xyz>
2021-11-01 21:26:29 +0000

    Take image from command line

--- src/atsim.c
+++ src/atsim.c
@@ -1,15 +1,31 @@
 #include <stdio.h>
 #include "machine.h"
 
-int main(void)
+int main(int argc, char* argv[])
 {
     // TODO: implement 32 bit instructions like STS
+
+    // Check args
+    if (argc != 2)
+    {
+        puts("Pass single input file.");
+        return 1;
+    }
+
     Machine m;
-    load_memory_from_file(&m, "test/fib/fib.bin");
+
+    if(!load_memory_from_file(&m, argv[1]))
+    {
+        return 1;
+    }
+
     m.PC = 0;
     m.SKIP = false;
+
     run_until_halt_loop(&m);
+
     dump_registers(&m);
     dump_stack(&m);
+
     return 0;
 }
